5310441cb8e553e969c01c00fb00df96ff1e52ae
kantegory <davidobryakov@gmail.com>
2020-03-01 16:27:15 +0300

    Migrate to bottle-py

--- main.py
+++ main.py
@@ -1,39 +1,31 @@
-import requests
-from bs4 import BeautifulSoup
+import bottle
+from bottle import request, response, route, template, static_file
+import json
 
 
-def get_page(url):
-    try:
-        headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
-        response = requests.get(url, headers=headers, verify=False)
-
-        if response.ok:
-            return response.text
-        else:
-            print("Error " + str(response.status_code))
-            return False
-    except requests.exceptions.ConnectTimeout:
-        print('Oops. Connection timeout occured!')
-    except requests.exceptions.ReadTimeout:
-        print('Oops. Read timeout occured')
-    except requests.exceptions.ConnectionError:
-        print('Seems like dns lookup failed..')
+@route('/admin')
+def get_admin_page():
+    return template('assets/admin.tpl')
 
 
-def extract_standards(page):
-    """ Extract standards from a given web page """
-    standards = [page.findAll('input', {'class': 'button btn'})]
-    return standards
+@route('/admin/blank/')
+def get_admin_blank_page():
+    return template('assets/blank.tpl')
 
 
-def main():
-    url = "https://profstandart.rosmintrud.ru/obshchiy-informatsionnyy-blok/" \
-          "natsionalnyy-reestr-professionalnykh-standartov/reestr-professionalnykh-standartov/"
+@route('/admin/report/')
+def get_admin_report_page():
+    return template('assets/report.tpl')
 
-    response = get_page(url)
-    page = BeautifulSoup(response, 'html5lib')
-    standards = extract_standards(page)
-    print(standards)
+
+@route('/quiz')
+def get_quiz_page():
+    return template('assets/quiz.tpl')
+
+
+def main(_host="localhost"):
+    app = bottle.app()
+    bottle.run(app=app, host=_host, port=8082)
 
 
 if __name__ == "__main__":
