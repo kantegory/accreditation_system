cd84c26d6a5670afc1f682825eb5fc8620a76ba2
kantegory <davidobryakov@gmail.com>
2020-03-09 22:08:26 +0300

    Try to fix encoding

--- utils/parse_xml.py
+++ utils/parse_xml.py
@@ -8,7 +8,9 @@ def get_content_by_tag(page, tag):
 
 def get_text_by_tag(page, tag):
     result = page.findAll(tag)
-    result = [res.get_text() for res in result] if len(result) > 1 else result[0].get_text()
+    result = [bytes(res.get_text().encode('utf-8')).decode('utf-8') for res in result] if len(result) > 1 else result[0].get_text()
+    # result = [res.encode(encoding='utf-8') for res in result]
+    print(result)
     return result
 
 
@@ -53,7 +55,7 @@ def get_particular_work_function(page):
 def parse_xml(filename):
     file = open(filename, 'r')
     xml = file.read()
-    page = BeautifulSoup(xml, 'lxml', from_encoding="UTF-8")
+    page = BeautifulSoup(xml, 'lxml')
     generalized_work_functions = get_generalized_work_functions(page)
 
     return generalized_work_functions
